b23a04a1f173273dcc3e3b4b6d3463883e18a002
alayzcer <alayzcer@gmail.com>
2023-05-17 20:24:24 +0300

    init commit

---
+++ mylib/include/mylib/mylib.hpp
@@ -0,0 +1,9 @@
+#ifndef __mylib_hpp__
+#define __mylib_hpp__
+#include <vector>
+
+namespace mylib {
+    void say1(std::vector<int> const&);
+}
+
+#endif
---
+++ mylib/sources/mylib.cpp
@@ -0,0 +1,7 @@
+#include "mylib/mylib.hpp"
+
+namespace mylib {
+    void say1(std::vector<int> const&) {
+        ;;
+    }
+}
